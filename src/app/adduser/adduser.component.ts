eb76c01be2533208be3c13e9826bb3079cbaf1fd
Ronak Mehta <46737666+meta-ronak-mehta@users.noreply.github.com>
2019-03-07 00:53:27 +0530

    EAD-Assignment-10

---
+++ src/app/adduser/adduser.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-adduser',
+  templateUrl: './adduser.component.html',
+  styleUrls: ['./adduser.component.css']
+})
+export class AdduserComponent implements OnInit {
+
+  
+
+  constructor(private userService:UserService) { }
+
+  ngOnInit() {
+  }
+
+  user:User;
+
+  save(name:String,contact:number,place:String):void{
+    this.user=new User();
+    this.user.name=name;
+    this.user.place=place;
+    this.user.contact=contact;
+    this.userService.insert(this.user);
+  }
+ 
+}
